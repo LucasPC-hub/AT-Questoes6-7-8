cfe80bb4097f04bfe132efb938fdab232d635b91
Lucas <lucas.pc99@hotmail.com>
2024-10-01 13:20:40 -0300

    forçando o ClientTest a rodar na porta 8080:8080 para evitar erros

--- ConsumirApi/src/test/java/edu/infnet/consumirapi/client/PessoaClientTest.java
+++ ConsumirApi/src/test/java/edu/infnet/consumirapi/client/PessoaClientTest.java
@@ -1,5 +1,8 @@
 package edu.infnet.consumirapi.client;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import edu.infnet.consumirapi.model.Pessoa;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,15 +11,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 @Testcontainers
 @SpringBootTest
 public class PessoaClientTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PessoaClientTest.class);
 
     @Autowired
     private PessoaClient pessoaClient;
@@ -24,7 +34,9 @@ public class PessoaClientTest {
     @Container
     public static GenericContainer<?> apiContainer = new GenericContainer<>("pessoasapi:latest")
             .withExposedPorts(8080)
-            .waitingFor(Wait.forHttp("/pessoas").forStatusCode(200));
+            .withCreateContainerCmdModifier(cmd -> cmd.withPortBindings(new PortBinding(Ports.Binding.bindPort(8080), new ExposedPort(8080))))
+            .waitingFor(Wait.forHttp("/pessoas").forStatusCode(200).withStartupTimeout(Duration.ofMinutes(2)))
+            .withLogConsumer(new Slf4jLogConsumer(LOGGER));
 
     @DynamicPropertySource
     static void registerApiProperties(DynamicPropertyRegistry registry) {
